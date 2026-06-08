79ded14f7426486d738d6fece2837e1f0898bc3b
Jacinto Valentino <jacinto.valentino27@gmail.com>
2026-06-08 23:52:43 +0200

    refactor(ui): use props.theme in BottomNavBar styles

--- libraries/kordo-ui/src/components/organisms/BottomNavBar/BottomNavBar.styles.ts
+++ libraries/kordo-ui/src/components/organisms/BottomNavBar/BottomNavBar.styles.ts
@@ -1,22 +1,21 @@
 import styled from '@emotion/native';
 import Animated from 'react-native-reanimated';
-import { theme } from 'theme';
 
-export const Container = styled.View(({ theme }) => ({
+export const Container = styled.View((props) => ({
   position: 'absolute',
   bottom: 20,
   width: '92%',
   left: '4%',
   height: 72,
   paddingHorizontal: 16,
-  borderRadius: theme.borderRadius.rounded,
-  backgroundColor: theme.colors.neutral.white,
+  borderRadius: props.theme.borderRadius.rounded,
+  backgroundColor: props.theme.colors.neutral.white,
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
 
   // iOS shadow
-  shadowColor: theme.colors.neutral.black,
+  shadowColor: props.theme.colors.neutral.black,
   shadowOffset: {
     width: 0,
     height: 2,
@@ -28,16 +27,16 @@ export const Container = styled.View(({ theme }) => ({
   elevation: 3,
 }));
 
-export const Tab = styled.TouchableOpacity<{ isAction?: boolean }>(({ theme }) => ({
+export const Tab = styled.TouchableOpacity<{ isAction?: boolean }>((props) => ({
   flex: 1,
   alignItems: 'center',
   justifyContent: 'center',
 }));
 
-export const highlightedBar = styled(Animated.View)(({ theme }) => ({
+export const highlightedBar = styled(Animated.View)((props) => ({
   position: 'absolute',
   top: 0,
   height: 4,
-  borderRadius: theme.borderRadius.rounded,
-  backgroundColor: theme.colors.primary.base,
+  borderRadius: props.theme.borderRadius.rounded,
+  backgroundColor: props.theme.colors.primary.base,
 }));
